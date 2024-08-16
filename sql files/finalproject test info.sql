5bd25b0bd2a51852d2855ebf75f7ad3bd1b02eab
lordYorden <87636389+lordYorden@users.noreply.github.com>
2024-08-16 10:09:32 +0300

    removed unnecessary sql files

--- sql files/finalproject test info.sql
+++
@@ -1,7 +0,0 @@
-insert into type (name) values ('Long Answer');
-select * from type;
-
-insert into answer (typeid, text) values (1, 'The quick brown fox jumps over the lazy dog')
-select * from answer;
-
-select text from answer where aid = 1;
